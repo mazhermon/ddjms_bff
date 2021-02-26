200a710fe41fb83ef597268f1ed90862652824df
Maz Hermon <mazhermon@MacBook-Pro.local>
2021-02-26 22:07:31 +1300

    change root route message

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const { urlencoded } = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
 
-// const DBATLAS = `mongodb+srv://user:${process.env.DBACCESS}@cluster0-vwya2.mongodb.net/${process.env.DBUSER}?retryWrites=true&w=majority`
 const DBATLAS = `mongodb+srv://user:${process.env.DBACCESS}@ddjmscluster1.tsps8.mongodb.net/${process.env.DBUSER}?retryWrites=true&w=majority`;
-//const DBATLAS = `mongodb+srv://user:<password>@ddjmscluster1.tsps8.mongodb.net/<dbname>?retryWrites=true&w=majority`
 const DBLOCAL = 'mongodb://localhost/dadjamslocaldb'
 
 mongoose.connect(process.env.MONGODB_URI || DBATLAS, {
@@ -33,7 +31,7 @@ app.use('/api', jamRoutes)
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'base api route hit, use /api for all the jams routes'
+    message: 'Kia Ora, this is Ddjms api for our jams app'
   })
 })
 
